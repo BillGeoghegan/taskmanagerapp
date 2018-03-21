2e895f3a89a17a741c14163212b2597d07b74cdd
BillGeoghegan <shadagascavich@gmail.com>
2018-03-21 16:16:15 +0000

    Adding filters and improving styles

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TasksSummary from './TasksSummary';
 import TaskList from './TaskList';
+import TaskListFilters from './TaskListFilters';
 
 const DashboardPage = () => (
     <div>
         <TasksSummary/>
+        <TaskListFilters/>
         <TaskList />
     </div>
 );
