5b603355b89bde886129ee750f6ec39f674c7a4f
BillGeoghegan <shadagascavich@gmail.com>
2018-03-22 18:19:26 +0000

    Adding due period function

--- src/components/TaskListFilters.js
+++ src/components/TaskListFilters.js
@@ -25,36 +25,38 @@ export class TaskListFilters extends React.Component {
     render() {
         return(
             <div className="content-container">
-                <div className="input-group">
-                    <div className="input-group__item">           
-                        <input 
-                            type="text" 
-                            className="text-input"
-                            value={this.props.filters.text} 
-                            placeholder="Search Tasks..."
-                            onChange={this.onTextChange}
-                        />
-                    </div>
-                    <div className="input-group__item">
-                        <select 
-                            className="select"
-                            value={this.props.filters.sortBy} 
-                            onChange={this.onSortChange}
-                        >
-                            <option value="date">Date</option>
-                        </select>
-                    </div>
-                    <div className="input-group__item">
-                        <DateRangePicker 
-                            startDate={this.props.filters.startDate}
-                            endDate={this.props.filters.endDate}
-                            onDatesChange={this.onDatesChange}
-                            focusedInput={this.state.calendarFocused}
-                            onFocusChange={this.onFocusChange}
-                            showClearDates={true}
-                            numberOfMonths={1}
-                            isOutsideRange={() => false}
-                        />
+                <div className="content-container-center">
+                    <div className="input-group">
+                        <div className="input-group__item">           
+                            <input 
+                                type="text" 
+                                className="text-input"
+                                value={this.props.filters.text} 
+                                placeholder="Search Tasks..."
+                                onChange={this.onTextChange}
+                            />
+                        </div>
+                        <div className="input-group__item">
+                            <select 
+                                className="select"
+                                value={this.props.filters.sortBy} 
+                                onChange={this.onSortChange}
+                            >
+                                <option value="date">Date</option>
+                            </select>
+                        </div>
+                        <div className="input-group__item">
+                            <DateRangePicker 
+                                startDate={this.props.filters.startDate}
+                                endDate={this.props.filters.endDate}
+                                onDatesChange={this.onDatesChange}
+                                focusedInput={this.state.calendarFocused}
+                                onFocusChange={this.onFocusChange}
+                                showClearDates={true}
+                                numberOfMonths={1}
+                                isOutsideRange={() => false}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -5,14 +5,39 @@ import numeral from 'numeral';
 
 const TaskListItem = ({dispatch, id, description = '', note = '', createdAt = 0, dueDate = 0, status = '', category = ''}) => {
     return (
-            <Link to={`/edit/${id}`} className="list-item">
-                <div>
-                    <h3 className="list-item__title">{description}</h3>
-                    <span className="list-item__sub-title">{note}</span>
-                </div>
-                <h4 className="list-item__data">{moment(dueDate).format('MMMM Do, YYYY')}</h4>
-            </Link>
+        <Link to={`/edit/${id}`} className="list-item">
+            <div>
+                <h3 className="list-item__title">{description} </h3>
+                <span className="list-item__sub-title">{note}</span>
+                <br/><br/>{statusColor({status})}
+            </div>
+            <h4 className="list-item__data"><span className="show-for-mobile">Due </span>{moment(dueDate).format('MMMM Do, YYYY')} {dateStatus({dueDate,description})}</h4>
+        </Link>
     );
 }
+
+const statusColor = ({status}) => {
+    let statusClass = 'list-item__sub-title bad';
+    if(status == 'Created' || status == 'Ongoing' || status == 'Complete'){
+        statusClass = 'list-item__sub-title good';
+    }else if(status == 'Closed'){
+        statusClass = 'list-item__sub-title neutral';
+    }
+    return(
+        <span className={statusClass}>{status}</span>
+    );
+};
+
+const dateStatus = ({dueDate,description}) => {
+    let overdueMessage = '(Overdue)';
+    if(moment(dueDate).unix() < moment().unix()){
+        overdueMessage = '(Overdue)';
+    }else{
+        overdueMessage = '(Due in '+moment(dueDate).toNow(true)+')';
+    }
+    return(
+        overdueMessage
+    );
+};
 
 export default TaskListItem;
